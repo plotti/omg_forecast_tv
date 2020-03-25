06fa5a5760142382500dc7e132aca5c058b73f14
Thomas Ebermann <thomas.ebermann@liip.ch>
2020-03-25 16:15:55 +0100

    setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"plotti@gmx.net\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\ 
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
